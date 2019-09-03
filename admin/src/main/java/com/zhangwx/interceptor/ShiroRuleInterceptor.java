77de6fc7e8071f162c4ee9dc9e9ba548c4b5b53c
zhangwx <1306562173@qq.com>
2019-09-03 10:54:37 +0800

    该拦截器暂时没用到

--- admin/src/main/java/com/zhangwx/interceptor/ShiroRuleInterceptor.java
+++ admin/src/main/java/com/zhangwx/interceptor/ShiroRuleInterceptor.java
@@ -15,30 +15,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.Optional;
 
+/**
+ * 拦截器，
+ */
 public class ShiroRuleInterceptor extends HandlerInterceptorAdapter {
 
-    @Autowired
-    private SysUserService sysUserService;
-
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        System.out.println("++++++++++++="+sysUserService);
-        SysRole sysRole=sysUserService.selectSysRoleByPrimaryKey(UserRequest.getCurrentRoleId());
-        Optional.ofNullable(sysRole).orElseThrow(() -> new ServiceException(MyExceptionCode.SYS_ROLE_NOT_EXIST));
-        if (sysRole.getDeleted() == EnumSysRole.DELETED_NO.getCode()){
-            throw new ServiceException(MyExceptionCode.SYS_DENY);
-        }
-        Subject subject = SecurityUtils.getSubject();
-        if(subject.hasRole(sysRole.getCode())) {
-            // 有权限，执行相关业务
-            return true;
-        } else {
-            // 无权限，给相关提示
-            throw new ServiceException(MyExceptionCode.SYS_DENY);
-//            return false;
-        }
-    }
-
+//    @Autowired
+//    private SysUserService sysUserService;
 
 
 }
